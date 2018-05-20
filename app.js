ec52990c522282027e94ba0699bdce72df13783e
Kushal Cuttari <kcutari@gmail.com>
2018-05-20 02:12:10 -0700

    Added a bunch of functionality to app.js

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456',
-    database: 'nodemysql'
+    password: 'root',
+    database: 'nodemysql',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
 // Connect
 db.connect((err) => {
     if (err) {
-        console.log("Error connecting to DB");
+        console.log("Error connecting to DB " + err);
     }
     console.log('MySql Connected...');
 });
@@ -28,6 +29,70 @@ app.get('/createdb', (req, res) => {
         res.send('Database created...');
     });
 });
+
+//Create Table
+app.get('/createpoststable', (req,res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), author VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if(err) console.log("Error creating table " + err );
+        console.log(result);
+        res.send("Posts Table Created...");
+    })
+});
+
+// Insert post 1
+app.get('/addpost1', (req, res) => {
+    let post = { title: 'Post One', body: 'This is post number one' };
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    });
+});
+
+// Select posts
+app.get('/getposts', (req, res) => {
+    let sql = 'SELECT * FROM posts';
+    let query = db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        res.send('Posts fetched...');
+    });
+});
+
+// Select single post
+app.get('/getpost/:id', (req, res) => {
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post fetched...');
+    });
+});
+
+// Update post
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post updated...');
+    });
+});
+
+// Delete post
+app.get('/deletepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Post deleted...');
+    });
+});
+
 
 //Use EJS
 app.engine('html', require('ejs').renderFile);
